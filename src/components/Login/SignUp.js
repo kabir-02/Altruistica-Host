cc3b86d41d9ce529cad35cfe5a310ce0ba17f6c7
Vritika Naik <codefinite-vri@users.noreply.github.com>
2021-06-04 22:50:00 +0530

    Updated Frontend

---
+++ src/components/Login/SignUp.js
@@ -0,0 +1,40 @@
+import React from "react";
+import loginImg from "../../img/login.svg";
+
+export class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="base-container" ref={this.props.containerRef}>
+        <div className="header">Sign Up</div>
+        <div className="content">
+          <div className="image">
+            <img src={loginImg} />
+          </div>
+          <div className="form">
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" placeholder="Email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="text" name="password" placeholder="Password" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="confirm-password">Confirm Password</label>
+              <input type="text" name="confirm-password" placeholder="Password" />
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Register
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
