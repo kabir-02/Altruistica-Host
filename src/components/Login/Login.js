c127e592b652522e5935fcd13c853f7ecce67692
Vritika Naik <codefinite-vri@users.noreply.github.com>
2021-06-11 15:32:46 +0530

    Updated Signup/Login with more inputs

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -16,11 +16,11 @@ export class Login extends React.Component {
           </div>
           <div className="form">
             <div className="form-group">
-              <label htmlFor="username">Email</label>
+              <label className="label-sign" htmlFor="username">Email</label>
               <input type="email" name="email" placeholder="Email" />
             </div>
             <div className="form-group">
-              <label htmlFor="password">Password</label>
+              <label className="label-sign" htmlFor="password">Password</label>
               <input type="password" name="password" placeholder="Password" />
             </div>
           </div>
--- src/components/Login/SignUp.js
+++ src/components/Login/SignUp.js
@@ -1,5 +1,6 @@
 import React from "react";
 import loginImg from "../../img/login.svg";
+import Grid from '@material-ui/core/Grid';
 
 export class SignUp extends React.Component {
   constructor(props) {
@@ -12,21 +13,48 @@ export class SignUp extends React.Component {
         <div className="header">Sign Up</div>
         <div className="content">
           <div className="image">
-            <img src={loginImg} />
+            <img className="img-sign" src={loginImg} />
           </div>
           <div className="form">
+          <Grid container spacing={3} >
+            <Grid item xs={12} sm={6}> 
             <div className="form-group">
-              <label htmlFor="email">Email</label>
+              <label className="label-sign" htmlFor="name">Name</label>
+              <input type="text" name="name" placeholder="Name" />
+            </div>
+            <div className="form-group">
+              <label className="label-sign" htmlFor="email">Email</label>
               <input type="text" name="email" placeholder="Email" />
             </div>
             <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input type="text" name="password" placeholder="Password" />
+              <label className="label-sign" htmlFor="password">Password</label>
+              <input type="password" name="password" placeholder="Password" />
             </div>
             <div className="form-group">
-              <label htmlFor="confirm-password">Confirm Password</label>
-              <input type="text" name="confirm-password" placeholder="Password" />
+              <label  className="label-sign" htmlFor="confirm-password">Confirm Password</label>
+              <input type="password" name="confirm-password" placeholder="Password" />
             </div>
+            </Grid>
+            <Grid item xs={12} sm={6}> 
+            <div className="form-group">
+              <label className="label-sign" htmlFor="country">Phone</label>
+              <input type="text" name="country" placeholder="Phone" />
+            </div>
+            <div className="form-group">
+              <label className="label-sign" htmlFor="city">City</label>
+              <input type="text" name="city" placeholder="City" />
+            </div>
+            <div className="form-group">
+              <label className="label-sign" htmlFor="State">State</label>
+              <input type="text" name="state" placeholder="State" required/>
+            </div>
+            <div className="form-group">
+              <label className="label-sign" htmlFor="country">Country</label>
+              <input type="text" name="country" placeholder="Country" />
+            </div>
+            
+            </Grid>
+            </Grid>
           </div>
         </div>
         <div className="footer">
